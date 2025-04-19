8795be374b7e016a9ccacd3ee5072a13bbe00344
Takuho NAKANO <takotakot@users.noreply.github.com>
2025-04-19 12:52:44 +0000

    Update functions.ts with TSDoc comments for all functions

--- functions.ts
+++ functions.ts
@@ -1,14 +1,26 @@
-// 常に 0 を返す関数
+/**
+ * 常に 0 を返す関数。
+ * @param n - 任意の整数。
+ * @returns 常に 0 を返す。
+ */
 export function sum_to_n(n: number): number {
     return 0;
 }
 
-// 常に与えられた配列を返す関数
+/**
+ * 常に与えられた配列を返す関数。
+ * @param array - 任意の整数配列。
+ * @returns 入力された配列をそのまま返す。
+ */
 export function bubble_sort(array: number[]): number[] {
     return array;
 }
 
-// 常に 0 を返す関数
+/**
+ * 常に 0 を返す関数。
+ * @param n - 任意の整数。
+ * @returns 常に 0 を返す。
+ */
 export function fibonacci(n: number): number {
     return 0;
 }
