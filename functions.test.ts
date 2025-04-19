ef0922bec8a16be38201a7c5336a4d377e0f682e
takotakot <takotakot@users.noreply.github.com>
2025-04-19 13:12:21 +0000

    Add failing test for bubble_sort function

--- functions.test.ts
+++ functions.test.ts
@@ -1,7 +1,13 @@
-import { sum_to_n } from './functions';
+import { sum_to_n, bubble_sort } from './functions';
 
 describe('sum_to_n', () => {
     test('should return 55 for input 10', () => {
         expect(sum_to_n(10)).toBe(55);
+    });
+});
+
+describe('bubble_sort', () => {
+    test('should sort the array [5, 3, 8, 1, 2] to [1, 2, 3, 5, 8]', () => {
+        expect(bubble_sort([5, 3, 8, 1, 2])).toEqual([1, 2, 3, 5, 8]);
     });
 });
