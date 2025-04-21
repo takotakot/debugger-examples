d5c77b8c28cabcfef1aa7ab96e1541e77f01a0ed
takotakot <takotakot@users.noreply.github.com>
2025-04-21 04:14:27 +0000

    Implement correct bubble_sort function and pass the test

--- functions.rb
+++ functions.rb
@@ -8,6 +8,14 @@ end
 # bubble_sort.rb
 
 def bubble_sort(array)
+  n = array.length
+  (0...n).each do |i|
+    (0...(n - i - 1)).each do |j|
+      if array[j] > array[j + 1]
+        array[j], array[j + 1] = array[j + 1], array[j]
+      end
+    end
+  end
   array
 end
 
