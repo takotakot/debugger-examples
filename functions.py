2a96aa0eb4db97125039dbb68c47ce7d9db608a3
takotakot <takotakot@users.noreply.github.com>
2025-04-26 07:50:59 +0000

    Implement correct bubble_sort function and pass the test

--- functions.py
+++ functions.py
@@ -9,8 +9,13 @@ def sum_to_n(n):
 
 def bubble_sort(array):
     """
-    常に与えられた配列を返す関数
+    整数の配列を与えると、バブルソートを行い、ソート済みの配列を返す
     """
+    n = len(array)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
     return array
 
 def fibonacci(n):
