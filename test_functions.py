52fe904f9f855cf4df142d9b565e682db3ff87ef
takotakot <takotakot@users.noreply.github.com>
2025-04-26 07:50:37 +0000

    Add failing test for bubble_sort function

--- test_functions.py
+++ test_functions.py
@@ -1,9 +1,13 @@
 import unittest
-from functions import sum_to_n
+from functions import sum_to_n, bubble_sort
 
 class TestSumToN(unittest.TestCase):
     def test_sum_to_n(self):
         self.assertEqual(sum_to_n(10), 55)
+
+class TestBubbleSort(unittest.TestCase):
+    def test_bubble_sort(self):
+        self.assertEqual(bubble_sort([5, 3, 8, 1, 2]), [1, 2, 3, 5, 8])
 
 if __name__ == "__main__":
     unittest.main()
