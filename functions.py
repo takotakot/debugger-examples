455ba4304eed25797e545762fe2f9be40fbece32
takotakot <takotakot@users.noreply.github.com>
2025-04-26 07:39:25 +0000

    Add initial implementations for sum_to_n, bubble_sort, and fibonacci functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+def sum_to_n(n):
+    """
+    常に 0 を返す関数
+    """
+    return 0
+
+def bubble_sort(array):
+    """
+    常に与えられた配列を返す関数
+    """
+    return array
+
+def fibonacci(n):
+    """
+    常に 0 を返す関数
+    """
+    return 0
