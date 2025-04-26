05b0a1b4df9231bd9f7a941615702da650474d09
takotakot <takotakot@users.noreply.github.com>
2025-04-26 07:53:16 +0000

    Add failing test for fibonacci function

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,5 @@
 import unittest
-from functions import sum_to_n, bubble_sort
+from functions import sum_to_n, bubble_sort, fibonacci
 
 class TestSumToN(unittest.TestCase):
     def test_sum_to_n(self):
@@ -8,6 +8,10 @@ class TestSumToN(unittest.TestCase):
 class TestBubbleSort(unittest.TestCase):
     def test_bubble_sort(self):
         self.assertEqual(bubble_sort([5, 3, 8, 1, 2]), [1, 2, 3, 5, 8])
+
+class TestFibonacci(unittest.TestCase):
+    def test_fibonacci(self):
+        self.assertEqual(fibonacci(8), 21)
 
 if __name__ == "__main__":
     unittest.main()
