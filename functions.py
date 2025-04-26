48564773c02bc7c81f118b6093428758ccf89842
takotakot <takotakot@users.noreply.github.com>
2025-04-26 07:45:12 +0000

    Implement correct sum_to_n function and pass the test

--- functions.py
+++ functions.py
@@ -1,8 +1,11 @@
 def sum_to_n(n):
     """
-    常に 0 を返す関数
+    正の整数 n を与えると、1 から n までの合計を計算して返す
+    正の整数以外が与えられた場合は 0 を返す
     """
-    return 0
+    if n <= 0 or not isinstance(n, int):
+        return 0
+    return sum(range(1, n + 1))
 
 def bubble_sort(array):
     """
