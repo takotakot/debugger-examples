0ac0163e179a9c5f9fa04e7b0fb764510346fc80
takotakot <takotakot@users.noreply.github.com>
2025-04-27 15:38:40 +0000

    Add initial implementations of sum_to_n, bubble_sort, and fibonacci functions

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+// 常に 0 を返す関数
+function sum_to_n($n) {
+    return 0;
+}
+
+// 常に与えられた配列を返す関数
+function bubble_sort($array) {
+    return $array;
+}
+
+// 常に 0 を返す関数
+function fibonacci($n) {
+    return 0;
+}
