e883bd16921c346a8fd3534a40bba74c844d9805
takotakot <takotakot@users.noreply.github.com>
2025-04-27 15:39:06 +0000

    Add PHPDoc comments to functions for better documentation

--- functions.php
+++ functions.php
@@ -1,16 +1,31 @@
 <?php
 
-// 常に 0 を返す関数
+/**
+ * 常に 0 を返す関数
+ *
+ * @param int $n 入力整数
+ * @return int 常に 0 を返す
+ */
 function sum_to_n($n) {
     return 0;
 }
 
-// 常に与えられた配列を返す関数
+/**
+ * 常に与えられた配列を返す関数
+ *
+ * @param array $array 入力配列
+ * @return array 入力された配列をそのまま返す
+ */
 function bubble_sort($array) {
     return $array;
 }
 
-// 常に 0 を返す関数
+/**
+ * 常に 0 を返す関数
+ *
+ * @param int $n 入力整数
+ * @return int 常に 0 を返す
+ */
 function fibonacci($n) {
     return 0;
 }
