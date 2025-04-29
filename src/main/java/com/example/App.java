79f25bff8217bd1b74be6c520cff3d2dd915c071
takotakot <takotakot@users.noreply.github.com>
2025-04-29 14:41:56 +0000

    Implement correct bubble_sort function and pass the test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -35,6 +35,16 @@ public class App
      * @return the sorted array
      */
     public static int[] bubble_sort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
         return array;
     }
 
