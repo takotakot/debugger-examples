17ed6f997941af9688c393937053c6821fff3daf
takotakot <takotakot@users.noreply.github.com>
2025-04-29 14:39:22 +0000

    Add JavaDoc comments and adjust indentation in App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,6 +11,12 @@ public class App
         System.out.println( "Hello World!" );
     }
 
+/**
+     * Calculates the sum of integers from 1 to n.
+     *
+     * @param n the upper limit of the range (inclusive)
+     * @return the sum of integers from 1 to n, or 0 if n is less than or equal to 0
+     */
     public static int sum_to_n(int n) {
         if (n <= 0) {
             return 0;
@@ -22,10 +28,22 @@ public class App
         return total;
     }
 
+    /**
+     * Sorts an array of integers using the bubble sort algorithm.
+     *
+     * @param array the array of integers to be sorted
+     * @return the sorted array
+     */
     public static int[] bubble_sort(int[] array) {
         return array;
     }
 
+    /**
+     * Calculates the nth Fibonacci number using recursion.
+     *
+     * @param n the position in the Fibonacci sequence (1-based index)
+     * @return the nth Fibonacci number, or 0 if n is less than or equal to 0
+     */
     public static int fibonacci(int n) {
         return 0;
     }
