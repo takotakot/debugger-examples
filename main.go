f2296355e8f26e1f093569aa240bc8b78138c86f
takotakot <takotakot@users.noreply.github.com>
2025-05-24 10:56:17 +0000

    ダミー実装: sum_to_n, bubble_sort, fibonacci の3関数を追加 (Go)

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+// sum_to_n: 常に0を返す
+func SumToN(n int) int {
+	return 0
+}
+
+// bubble_sort: 常に与えられた配列を返す
+func BubbleSort(array []int) []int {
+	return array
+}
+
+// fibonacci: 常に0を返す
+func Fibonacci(n int) int {
+	return 0
+}
