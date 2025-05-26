d2ed53ff809b7e780221137ea13bb2075901f9cf
takotakot <takotakot@users.noreply.github.com>
2025-05-26 00:46:34 +0000

    feat: ダミー実装（sum_to_n, bubble_sort, fibonacci）を追加

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+// Rust のサンプル実装
+
+pub fn sum_to_n(_n: i32) -> i32 {
+    0
+}
+
+pub fn bubble_sort<T: Clone>(array: &[T]) -> Vec<T> {
+    array.to_vec()
+}
+
+pub fn fibonacci(_n: i32) -> i32 {
+    0
+}
