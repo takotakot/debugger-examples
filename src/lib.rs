d07a24e7a3fb9d2a0432449d5b13b2d5448dfbcc
takotakot <takotakot@users.noreply.github.com>
2025-05-26 00:57:41 +0000

    Implement correct sum_to_n; test passes for input 10 == 55

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
 // Rust のサンプル実装
 
-pub fn sum_to_n(_n: i32) -> i32 {
-    0
+pub fn sum_to_n(n: i32) -> i32 {
+    if n <= 0 {
+        return 0;
+    }
+    (1..=n).sum()
 }
 
 pub fn bubble_sort<T: Clone>(array: &[T]) -> Vec<T> {
