fda882dba8040779bdae2175874d55a46226cc22
takotakot <takotakot@users.noreply.github.com>
2025-06-07 13:41:01 +0000

    C#用の3関数（sum_to_n, bubble_sort, fibonacci）を仮実装（全てダミー実装）

---
+++ DebuggerExamples/Functions.cs
@@ -0,0 +1,27 @@
+﻿namespace DebuggerExamples;
+
+public class Class1
+{
+
+}
+
+public static class Functions
+{
+    // 常に0を返す
+    public static int sum_to_n(int n)
+    {
+        return 0;
+    }
+
+    // 常に与えられた配列を返す
+    public static int[] bubble_sort(int[] array)
+    {
+        return array;
+    }
+
+    // 常に0を返す
+    public static int fibonacci(int n)
+    {
+        return 0;
+    }
+}
