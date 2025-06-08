8a6c322d60a6a88298aa3232069331124c60ed49
takotakot <takotakot@users.noreply.github.com>
2025-06-08 02:31:28 +0000

    feat: ダミー実装の3関数(sum_to_n, bubble_sort, fibonacci)を追加 (Haskell)

---
+++ Lib.hs
@@ -0,0 +1,17 @@
+module Lib (
+    sum_to_n,
+    bubble_sort,
+    fibonacci
+) where
+
+-- 常に 0 を返す関数
+sum_to_n :: Int -> Int
+sum_to_n _ = 0
+
+-- 常に与えられた配列を返す関数
+bubble_sort :: [Int] -> [Int]
+bubble_sort xs = xs
+
+-- 常に 0 を返す関数
+fibonacci :: Int -> Int
+fibonacci _ = 0
