b4ead6b4dc8af64db7c9a19c84e399abe81cec68
takotakot <takotakot@users.noreply.github.com>
2025-06-21 08:32:21 +0000

    sum_to_nのユニットテストを追加（失敗する状態）

--- test/debugger_examples_test.dart
+++ test/debugger_examples_test.dart
@@ -2,7 +2,7 @@ import 'package:debugger_examples/debugger_examples.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('sum_to_n returns 55 when input is 10', () {
+    expect(sum_to_n(10), 55);
   });
 }
