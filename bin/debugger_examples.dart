8b162a9879cee9f8b9ff1d4f649c510a70c84f26
takotakot <takotakot@users.noreply.github.com>
2025-06-21 08:30:37 +0000

    Dartプロジェクトの初期構成ファイル・ディレクトリを追加

---
+++ bin/debugger_examples.dart
@@ -0,0 +1,5 @@
+import 'package:debugger_examples/debugger_examples.dart' as debugger_examples;
+
+void main(List<String> arguments) {
+  print('Hello world: ${debugger_examples.calculate()}!');
+}
---
+++ test/debugger_examples_test.dart
@@ -0,0 +1,8 @@
+import 'package:debugger_examples/debugger_examples.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
