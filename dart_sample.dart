f3bffc1038f2234c1751804b96a9ddd0b27ec685
takotakot <takotakot@users.noreply.github.com>
2025-06-21 08:19:42 +0000

    feat: ダミー実装（sum_to_n, bubble_sort, fibonacci）を追加

---
+++ dart_sample.dart
@@ -0,0 +1,16 @@
+// dart_sample.dart
+
+// 常に0を返す関数
+int sum_to_n(int n) {
+  return 0;
+}
+
+// 常に与えられた配列を返す関数
+List<int> bubble_sort(List<int> array) {
+  return array;
+}
+
+// 常に0を返す関数
+int fibonacci(int n) {
+  return 0;
+}
