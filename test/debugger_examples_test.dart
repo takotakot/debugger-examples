65af7739646015c735e2d2876ca09bcd2c6b7e01
takotakot <takotakot@users.noreply.github.com>
2025-06-21 08:36:37 +0000

    fibonacci関数を正しい再帰実装に修正し、テストを追加

--- test/debugger_examples_test.dart
+++ test/debugger_examples_test.dart
@@ -9,4 +9,8 @@ void main() {
   test('bubble_sort sorts [5, 3, 8, 1, 2] to [1, 2, 3, 5, 8]', () {
     expect(bubble_sort([5, 3, 8, 1, 2]), [1, 2, 3, 5, 8]);
   });
+
+  test('fibonacci returns 21 when input is 8', () {
+    expect(fibonacci(8), 21);
+  });
 }
